c66f4b0f059f82e022f8de630cfd98f4dc634497
Jen Hu <jhu@berkeley.edu>
2015-04-04 16:10:24 -0400

    inital ui

--- components/AppComponent.js
+++ components/AppComponent.js
@@ -2,6 +2,25 @@ var React = require('React');
 
 module.exports = React.createClass({
   render: function() {
-    return <div>hai</div>;
+    return (
+    	<div>
+    	<form action = "">
+    	Enter organism: <input type="text" name="orgName" value="mbb" /><br />
+    	<em>NB: mbb = M. bovis BCG; mtv = M. tuberculosis H37Rv; msg = M. smegmatis MC2 155</em><br />
+    	Search gene description: <input type="text" name="geneDescription" value="hydroxylase" /><br />
+    	<input type="submit" value="Submit"/><br />
+    	</form>
+    	{/*rest.kgg/jp/find/[orgName]/[geneDescription]
+    	//return number of entries
+    	//can manually curate entries & delete
+    	//save entries in a list. upon saving, retrieve all
+    	//parameters: http://rest.kegg.jp/get/mbb:BCG_0816c/ including ntseq and aaseq
+		*/}
+    	<form action = "">
+    	Save gene entries? <input type="text" name="listName" value="list name"/><br/>
+    	<input type="submit" value="Submit" />
+    	</form>
+    	</div>
+    	);
   }
 });
