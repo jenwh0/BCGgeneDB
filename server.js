4f1c4819c795d9f6de6a172c3fb08af424929da9
jeffmo <lbljeffmo@gmail.com>
2015-04-04 18:03:45 -0400

    Auto-rebundle on page load

--- server.js
+++ server.js
@@ -13,17 +13,15 @@ var COMPONENTS_DIR = path.resolve(__dirname, 'components');
 var LIB_DIR = path.resolve(__dirname, 'lib');
 var PAGE_BOOTLOADER_PATH = path.resolve(__dirname, 'pageBootloader.js');
 
-
 function fetchJSBundleData() {
   return new Promise(function(res, rej) {
-    console.log('Finding all files under ./components/...');
+    console.log('Finding files to be bundled...');
     var compsGlob = path.resolve(COMPONENTS_DIR, '*.js');
     var libGlob = path.resolve(LIB_DIR, '*.js');
     glob('{' + compsGlob + ',' + libGlob + '}', null, function(err, files) {
       if (err) { rej(err); }
       console.log(' - Found ' + files.length + ' files.');
 
-      console.log('Building browserify instance...');
       var bInst = browserify({
         baseDir: COMPONENTS_DIR,
       });
@@ -58,65 +56,73 @@ function fetchJSBundleData() {
       var jsBundleData = '';
       bStream.on('data', function(chunk) { jsBundleData += chunk; });
       bStream.on('error', rej);
-      bStream.on('end', function() { res(jsBundleData); });
+      bStream.on('end', function() {
+        console.log('Finished bundling.');
+        res(jsBundleData);
+      });
     });
   });
 }
 
 var jsBundleData = null;
 function getJSBundleData() {
+  // TODO: Memoize the results if nothing has changed
   if (jsBundleData === null) {
-    jsBundleData = fetchJSBundleData();
+    console.log('Re-bundling files incase something changed...');
+    jsBundleData = fetchJSBundleData().then(function(data) {
+      jsBundleData = null;
+      return data;
+    });
   }
   return jsBundleData;
 }
 
-getJSBundleData().done(function(jsBundleData) {
-  console.log('Starting web server...');
+getJSBundleData();
 
-  var app = express();
-  app.get('/', function(req, res) {
-    res.send([
-      '<html>',
-      '  <head>',
-      '    <title>BCGgeneDB</title>',
-      '  </head>',
-      '  <body>',
-      '    <div id="toplevel"></div>',
-      '    <script src="http://fb.me/react-0.13.1.js"></script>',
-      '    <script src="/bundle.js"></script>',
-      '    <script type="text/javascript">',
-      '      require("./pageBootloader.js").startPage();',
-      '    </script>',
-      '  </body>',
-      '</html>',
-    ].join('\n'));
-  });
+console.log('Starting web server...');
+var app = express();
+app.get('/', function(req, res) {
+  res.send([
+    '<html>',
+    '  <head>',
+    '    <title>BCGgeneDB</title>',
+    '  </head>',
+    '  <body>',
+    '    <div id="toplevel"></div>',
+    '    <script src="http://fb.me/react-0.13.1.js"></script>',
+    '    <script src="/bundle.js"></script>',
+    '    <script type="text/javascript">',
+    '      require("./pageBootloader.js").startPage();',
+    '    </script>',
+    '  </body>',
+    '</html>',
+  ].join('\n'));
+});
 
-  app.get('/bundle.js', function(req, res) {
+app.get('/bundle.js', function(req, res) {
+  getJSBundleData().done(function(jsBundleData) {
     res.set('Content-Type', 'text/javascript');
     res.send(jsBundleData);
   });
-
-  app.get('/keggAPI/find/', function(req, res) {
-    var e_db = encodeURIComponent(req.query.db);
-    var e_query = encodeURIComponent(req.query.q);
-
-    var url = 'http://rest.kegg.jp/find/' + e_db + '/' + e_query;
-    console.log('Fetching from KEGG API: ' + url + '...');
-    fetch(url)
-      .then(function(response) {
-        return response.text();
-      })
-      .done(function(response) {
-        res.send(response);
-      });
-  });
-
-  var server = app.listen(3000, 'localhost', function() {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log(' - Webserver started at: http://%s:%s/', host, port);
-  });
 });
 
+app.get('/keggAPI/find/', function(req, res) {
+  var e_db = encodeURIComponent(req.query.db);
+  var e_query = encodeURIComponent(req.query.q);
+
+  var url = 'http://rest.kegg.jp/find/' + e_db + '/' + e_query;
+  console.log('Fetching from KEGG API: ' + url + '...');
+  fetch(url)
+    .then(function(response) {
+      return response.text();
+    })
+    .done(function(response) {
+      res.send(response);
+    });
+});
+
+var server = app.listen(3000, 'localhost', function() {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Webserver started at: http://%s:%s/', host, port);
+});
